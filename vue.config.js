0a730fa01af905825dff250aac5257bf91ba72dd
example <example@gmail.com>
2021-03-01 17:07:17 +0800

    修改配置文件接口地址，生产环境打包不生成sourceMap

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,7 @@ const IS_PROD = ["production", "prod"].includes(process.env.NODE_ENV);
 
 module.exports = {
   publicPath: IS_PROD ? "./" : "/",
+  productionSourceMap: false,
   chainWebpack: (config) => {
     // 修复HMR
     config.resolve.symlinks(true);
